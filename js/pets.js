f822bb86eb98d017b1d2cbd3295e380d3137693e
Snedden Gonsalves <sgg3590@rit.edu>
2015-09-25 10:42:52 -0400

    selectBreedWorking

--- js/pets.js
+++ js/pets.js
@@ -3,7 +3,7 @@ function pets(){
 	var baseData;
 	var apiSecret;
 	var breeds;
-	
+	var breed;
 	
 	function construct()
 	{
@@ -23,12 +23,14 @@ function pets(){
 	breeds=[];
 	setListeners();
 	
-	getData();
+	//getData();
 	}
 	
 	function setListeners()
 	{
 	document.getElementById("animals").addEventListener("change",fetchBreeds);
+	
+	console.log('events added');
 	}
 	
 	
@@ -36,7 +38,8 @@ function pets(){
 		//var ajax;
 		//var data;
 		//ajax = new XMLHttpRequest();
-		console.log("animal :"+document.getElementById("animals").value);
+		//console.log("animal :"+document.getElementById("animals").value);
+		
 	   	 $.ajax({                    															//Asyn ajax to API
 			type: 'get',
 			url: 'http://api.petfinder.com/breed.list?key='+apiKey+'&format=json&animal='+document.getElementById("animals").value,
@@ -48,7 +51,9 @@ function pets(){
 				
 				var bindTo='breeds';
 				bindData(bindTo);
-                
+				
+				document.getElementById("breeds").addEventListener("change",function(){getData();});
+                getData();
 				
 
 			
@@ -60,21 +65,33 @@ function pets(){
 		});
 	}
 	
-	function getData(){
+	function getData(animal,breed){
+	animal=document.getElementById("animals").value;     
+	breeds=document.getElementById("breeds");
+    breed = breeds.options[breeds.selectedIndex].text;
+	console.log('Index '+breed.indexOf('/'));
+		if(breed.indexOf('/')>0){
+		breed=breed.split('/');
+		breed=breed[0];
+		console.log('b'+breed);
+		}
 	console.log('in get data');
 	var location=14623;
+	var URL='http://api.petfinder.com/pet.find?key='+apiKey+'&count=20&format=json&breed='+breed+'&animal='+animal+'&location='+location;
 	$.ajax({                    															//Asyn ajax to API
 			type: 'get',
-			url: 'http://api.petfinder.com/pet.find?key='+apiKey+'&format=json&location='+location,
+			url:  URL,
 			dataType: 'jsonp',
 			success: function(data)																//Callback function
 			{
-				console.log('data '+data);
+				console.log('request:'+ URL);
 				
 				formatPets(data);
+				console.log('animal '+animal);
+				console.log('breed '+breed);
+				loadImages();
 				
-				//bindData(breeds);
-             },
+			},
 			error: function( errorThrown)
 			{
 				console.log("Error making the request:"+errorThrown);
@@ -83,8 +100,66 @@ function pets(){
 	}
 	
 	function  formatPets(data){
+	baseData.length=0;
+	console.log(data);
+	console.log('pets '+data.petfinder.pets);
+		if(data.petfinder.pets.pet){
+			var pets=data.petfinder.pets.pet;
+			
+		
+			var length = pets.length;
+				
+
+				for(var i = 0; i < length; i++)
+				{
+					baseData.push({
+							'num'			:i,
+							'id'			:pets[i].id.$t,
+							'options'		:pets[i].options,
+							'breeds'		:pets[i].breeds.breed,
+							'status' 		:pets[i].status.$t,
+							'name"'			:pets[i].name.$t,
+							'contact' 		:pets[i].contact,
+							'description'	:pets[i].description.$t,
+							'sex'			:pets[i].sex.$t,
+							'age		'	:pets[i].age.$t,
+							'size'			:pets[i].size.$t,
+							'mix'			:pets[i].mix.$t,
+							'shelterId'		:pets[i].shelterId.$t ,
+							'lastUpdate'	:pets[i].lastUpdate.$t,
+							'media'	 		:pets[i].media,
+							'animal'		:pets[i].animal.$t		
+						});
+				}
+		}	
+		console.log(baseData);
 	
 	}
+	
+	function loadImages(){
+	
+	document.getElementById("imageList").innerHTML = "";
+	//
+	
+	
+	
+	
+	
+	var length=baseData.length;
+	var images='';
+	var imgSrc;
+	console.log(baseData);
+		for(var i=0;i<length;i++){
+			console.log("is data"+baseData[i].media.photos);
+			if(baseData[i].media.photos)
+			{
+			images+='<a href=""><img src="'+baseData[i].media.photos.photo[2].$t+'"width:"160" height:"160">';
+			}
+		}	
+	$('#imageList').append(images)
+	}
+	
+	
 	function bindData(bindTo){
 	
 	switch(bindTo){
